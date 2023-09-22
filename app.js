7f6c2ddeaedc9cf934c673276bdc4ee30ab9173a
RealZasin <113669153+RealZasin@users.noreply.github.com>
2023-09-22 18:18:37 +0200

    You can now append tasks(will not be saved)

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const button = document.getElementById('add-button');
 const inputField = document.getElementById('input-field');
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const appSettings = {
+  databaseURL: 'https://playground-bc12c-default-rtdb.europe-west1.firebasedatabase.app/'
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const toDoListInDB = ref(database, 'toDoList')
 
 button.addEventListener('click', function() {
   const inputValue = inputField.value;
-  console.log(inputValue);
+
+  push(toDoListInDB, inputValue)
+
+  console.log(`${inputValue} added to database`);
+
+  const li = document.createElement('li');
+  li.textContent = inputValue;
+  document.getElementById('itemList').appendChild(li);
+
 });
